da596cbea2528b012bb13250015efca6adbd9715
Travis Emslander <travis.emslander@gmail.com>
2018-08-25 17:25:18 -0500

    Initial commit

---
+++ content/posts/2018-08-25/index.md
@@ -0,0 +1,29 @@
+---
+title: "Minecraft Intro"
+cover: "https://res.cloudinary.com/lmn/image/upload/e_sharpen:100/f_auto,fl_lossy,q_auto/v1/gameskinnyc/u/n/t/untitled-a5150.jpg"
+author: "evan"
+date: "2018-08-25"
+category: "gaming"
+tags:
+    - minecraft
+---
+
+This is the jail. It's one of the smallest things in my village. My village is called FunIllage. ![jail]
+
+This is the enchantment room. It's the smallest thing in FunIllage. ![enchantment]
+
+This is the Firehouse. It has lots of undergroud things, like pools. ![firehouse-front]
+
+This is FunIllage. ![firehouse-top]
+
+This is my pet Meowy. ![pet-mowie]
+
+This is my pet Ruff. ![pet-ruff]
+
+[jail]: images/minecraft/jail.jpg "Minecraft Jail"
+[enchantment]: images/minecraft/enchantment.jpg "Enchantment Room"
+[firehouse-front]: images/minecraft/firehouse-front.jpg "Font of my Fire House"
+[firehouse-top]: images/minecraft/firehouse-top.jpg "Top of my Fire House"
+[pet-mowie]: images/minecraft/pet-mowie.jpg "My Pet Mowie"
+[pet-ruff]: images/minecraft/pet-ruff.jpg "My Pet Ruff"
+[waterslide]: images/minecraft/waterslide.jpg "Waterslide"
---
+++ data/SiteConfig.js
@@ -0,0 +1,35 @@
+module.exports = {
+  blogPostDir: "posts", // The name of directory that contains your posts.
+  blogAuthorDir: "tink-authors", // The name of directory that contains your 'authors' folder.
+  blogAuthorId: "evan", // The default and fallback author ID used for blog posts without a defined author.
+  siteTitle: "T-INK", // Site title.
+  siteTitleAlt: "T-INK", // Alternative site title for SEO.
+  siteLogo:
+    "/logos/tink-circle-512.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
+  siteUrl: "http://t-ink.org", // Domain of your website without pathPrefix.
+  pathPrefix: "/t-ink-page", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
+  siteDescription:
+    "T-INK Website.", // Website description used for RSS feeds/meta description tag.
+  siteCover:
+    "/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
+  siteNavigation: true, // If navigation is enabled the Menu button will be visible
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteRssAuthor: "Evan", // The author name used in the RSS file
+  // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
+  sitePaginationLimit: 10, // The max number of posts per page.
+  googleAnalyticsID: "", // GA tracking ID.
+  // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
+  siteSocialUrls: [],
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  // Links to social profiles/projects you want to display in the navigation bar.
+  userLinks: [],
+  // Copyright string for the footer of the website and RSS feed.
+  copyright: {
+    label: "T-INK" // Label used before the year
+    // year: "2018" // optional, set specific copyright year or range of years, defaults to current year
+    // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
+  },
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  promoteGatsby: true // Enables the GatsbyJS promotion information in footer.
+};
